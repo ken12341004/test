dfe8a0d6c53862c921d774d039422209eccf4f0a
ken12341004 <80029906+ken12341004@users.noreply.github.com>
2024-03-12 20:39:21 +0800

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,27 +1,41 @@
-var currentImage = 0;
-var totalImages = 72;
-var currentBatteryImage = 0;
-var totalBatteryImages = 5;
+window.onload = function() {
+  var overlay1 = document.getElementById("overlay");
+  var gif1 = document.getElementById("animated-gif-1");
+  var i1 = 0;
 
-var loadingIntervalId;
-var batteryIntervalId;
-
-function changeImage() {
-     currentImage = (currentImage + 1) % totalImages;
-     document.getElementById("currentImage").src = 'hero/' + currentImage.toString().padStart(2, '0') + '.png';
-
-     if (currentImage === totalImages - 1) {
-        clearInterval(loadingIntervalId);
-     }
-}
-
-function changeBatteryImage() {
-    currentBatteryImage = (currentBatteryImage + 1) % totalBatteryImages;
-    document.getElementById("batteryImage").src = '電池/' + currentBatteryImage + '.png';
-
-    if (currentBatteryImage === totalBatteryImages - 1) {
-        clearInterval(batteryIntervalId);
+  // Change the first gif source every 100ms
+  var gif1Interval = setInterval(function() {
+    if (i1 === 71) {
+      // Change the interval to Infinity to stop the animation
+      clearInterval(gif1Interval);
+      return;
     }
+    i1 = (i1 + 1) % 72;
+    gif1.src = "吉祥物/"+i1.toString().padStart(2, '0')+".png";
+  }, 50);
+
+  // Fade out the first overlay after 4 seconds
+  setTimeout(function() {
+    overlay1.style.opacity = 0;
+  }, 3200);
+
+  // Remove the first overlay after 4.2 seconds
+  setTimeout(function() {
+    overlay1.style.display = "none";
+  }, 3800);
+
+  var overlay2 = document.getElementById("overlay-2"); // overlay-2 has been removed, so this line can be commented out
+  var gif2 = document.getElementById("animated-gif-2");
+  var i2 = 0;
+
+  // Change the second gif source every 200ms
+  var gif2Interval = setInterval(function() {
+    if (i2 === 4) {
+      // Change the interval to Infinity to stop the animation
+      clearInterval(gif2Interval);
+      return;
+    }
+    i2 = (i2 + 1) % 5;
+    gif2.src = "電池/"+i2.toString().padStart(1, '0')+".png";
+  }, 600);
 }
-loadingIntervalId = setInterval(changeImage, 5);
-batteryIntervalId = setInterval(changeBatteryImage, 675);
