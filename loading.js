0c21b79a25a00328bc883d6f113847229ffcbe59
ken12341004 <80029906+ken12341004@users.noreply.github.com>
2024-03-13 23:40:03 +0800

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,41 +1,20 @@
-window.onload = function() {
-  var overlay1 = document.getElementById("overlay");
-  var gif1 = document.getElementById("animated-gif-1");
-  var i1 = 0;
+const loadingVideo = document.getElementById('loading-video');
 
-  // Change the first gif source every 100ms
-  var gif1Interval = setInterval(function() {
-    if (i1 === 71) {
-      // Change the interval to Infinity to stop the animation
-      clearInterval(gif1Interval);
-      return;
-    }
-    i1 = (i1 + 1) % 72;
-    gif1.src = "吉祥物/"+i1.toString().padStart(2, '0')+".png";
-  }, 50);
+setTimeout(function() {
+  const loadingPage = document.getElementById('loading-page');
+  loadingPage.style.transition = 'opacity 1s';
+  loadingPage.style.opacity = '0';
 
-  // Fade out the first overlay after 4 seconds
   setTimeout(function() {
-    overlay1.style.opacity = 0;
-  }, 3200);
+    loadingPage.parentNode.removeChild(loadingPage);
+    loadingVideo.pause(); 
+  }, 900);
+}, 3100);
 
-  // Remove the first overlay after 4.2 seconds
-  setTimeout(function() {
-    overlay1.style.display = "none";
-  }, 3800);
+loadingVideo.addEventListener('loadedmetadata', function() {
+  loadingVideo.play();
+});
 
-  var overlay2 = document.getElementById("overlay-2"); // overlay-2 has been removed, so this line can be commented out
-  var gif2 = document.getElementById("animated-gif-2");
-  var i2 = 0;
-
-  // Change the second gif source every 200ms
-  var gif2Interval = setInterval(function() {
-    if (i2 === 4) {
-      // Change the interval to Infinity to stop the animation
-      clearInterval(gif2Interval);
-      return;
-    }
-    i2 = (i2 + 1) % 5;
-    gif2.src = "電池/"+i2.toString().padStart(1, '0')+".png";
-  }, 600);
-}
+loadingVideo.addEventListener('ended', function() {
+  loadingVideo.pause();
+});
